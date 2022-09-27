a49f9ac7073e3731fb8e3a20873c51484fe4a005
Alonso Donayre <alonso2295@hotmail.com>
2022-09-26 20:29:40 -0500

    Remote backend s3

--- aws_projects/backend_state_file/main.tf
+++ aws_projects/backend_state_file/main.tf
@@ -2,3 +2,13 @@ resource "local_file" "soccer" {
   filename = "./data_out/soccer.txt"
   content = "I love soccer!" 
 }
+
+
+terraform {
+  backend "s3" {
+    bucket = "md-corp-terraform-state-bucket01"
+    key = "finance/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "state-locking"
+  }
+}
